c1f31e155a3f8db277198f1387b36005f9cdd1ad
nishealsohuwe <ay.ekimovskix@gmail.com>
2023-11-13 21:38:12 +0300

    Create models and some db
    In next time need fix web.config and StudentController and add this to project

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using GameLibrary.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+
+namespace GameLibrary.DAL
+{
+    public class SchoolContext: DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using GameLibrary.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GameLibraryContext") ?? throw new InvalidOperationException("Connection string 'GameLibraryContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
