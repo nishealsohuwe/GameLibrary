d34add9a23ff99af9bb2d1185499af6869c8f806
nishealsohuwe <ay.ekimovskix@gmail.com>
2023-11-13 19:52:52 +0300

    Initial commit

---
+++ Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic; 
+
+namespace GameLibrary.Models
+{
+    public class Game
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string developer { get; set; }
+        public virtual ICollection<Genre> Genres { get; set; }
+    }
+}
